456f498104d6131c45fefa4e5adaf0ef11378e82
Wyatt <Wyatt6@163.com>
2023-05-17 18:50:10 +0800

    菜单栏

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,8 +4,9 @@ import variables from '@/styles/common/variables.scss'
 
 // 用法：store.getters.XXX
 const getters = {
+  profile: (state) => state.common.profile,
   sidebarOpened: (state) => state.common.sidebarOpened,
-  cssVar: (state) => variables // 把scss中定义的样式参数导入vuex中方便使用
+  cssVar: () => variables // 把scss中定义的样式参数导入vuex中方便使用
 }
 
 export default getters

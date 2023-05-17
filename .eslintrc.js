2e92b2203bda89ca2a1d4a50f879a11dade5d25a
Wyatt <Wyatt6@163.com>
2023-05-17 12:39:17 +0800

    初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,29 @@
+// ESLint配置文件遵循commonJS的导出规则，所导出的对象就是ESLint的配置对象
+// 文档：https://eslint.bootcss.com/docs/user-guide/configuring
 module.exports = {
+  // 表示当前目录即为根目录，ESLint规则将被限制到该目录下
   root: true,
+  // env表示启用ESLint检测的环境
   env: {
-    node: true
+    // 在node环境下启动ESLint检测
+    node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  // ESLint中基础配置需要继承的配置
+  extends: ["plugin:vue/vue3-essential", "@vue/standard"],
+  // 解析器
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
+  /**
+   * 需要修改的启用规则及其各自的错误级别，错误级别分为三种：
+   * "off" 或 0 - 关闭规则
+   * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
+   * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+   */
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // 关闭函数名和括号之间插入空格的规则
+    "space-before-function-paren": "off",
+  },
+};
---
+++ vue.config.js
@@ -0,0 +1,51 @@
+// 路径解析的标准步骤
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+// https://cli.vuejs.org/zh/guide/webpack.html#简单的配置方式
+module.exports = {
+  // webpack提供了devServer代理功能，可以把所有请求到当前服务中的请求，转发到另外的服务器上
+  // devServer: {
+  //   proxy: {
+  //     // 当地址中有/api的时候会触发代理机制
+  //     '/api': {
+  //       // 要代理的服务器地址
+  //       target: process.env.SERVER,
+  //       changeOrigin: true // 是否跨域
+  //     }
+  //   }
+  // },
+  chainWebpack(config) {
+    // 设置svg-sprite-loader（解决项目自定义图标不显示的问题）
+    // config为webpack配置对象
+    // config.module表示创建一个具名规则，以后用来修改规则
+    config.module
+      // 规则
+      .rule('svg')
+      // 忽略
+      .exclude.add(resolve('src/assets/icons'))
+      // 结束
+      .end()
+    config.module
+      // 规则
+      .rule('icons')
+      // 正则，解析.svg格式文件
+      .test(/\.svg$/)
+      // 解析的文件
+      .include.add(resolve('src/assets/icons'))
+      // 结束
+      .end()
+      // 新增了一个解析的loader
+      .use('svg-sprite-loader')
+      // 具体的loader
+      .loader('svg-sprite-loader')
+      // loader的配置
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      // 结束
+      .end()
+  }
+}

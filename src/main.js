1d0df379b51c9d92af64d1dcc66ac00abca1590c
Wyatt <Wyatt6@163.com>
2023-05-17 12:45:39 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// vue add element-plus命令自动创建引入的
 import installElementPlus from './plugins/element'
 
 const app = createApp(App)

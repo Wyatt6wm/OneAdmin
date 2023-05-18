41fa995ab73d105470b76f86afbd0bd0c916a700
Wyatt <Wyatt6@163.com>
2023-05-18 08:45:05 +0800

    个人主页

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,10 @@ import variables from '@/styles/common/variables.scss'
 
 // 用法：store.getters.XXX
 const getters = {
+  token: (state) => state.common.token,
+  hasProfile: (state) => {
+    return JSON.stringify(state.common.profile) !== '{}' // true表示用户信息已存在；false表示不存在
+  },
   profile: (state) => state.common.profile,
   sidebarOpened: (state) => state.common.sidebarOpened,
   cssVar: () => variables // 把scss中定义的样式参数导入vuex中方便使用

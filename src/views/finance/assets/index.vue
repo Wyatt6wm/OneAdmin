0c1ad95e1db9531924c43528ff5392bfe9a73562
Wyatt <Wyatt6@163.com>
2023-05-20 09:31:38 +0800

    资产管理

--- src/views/finance/assets/index.vue
+++ src/views/finance/assets/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>资产管理</div>
+  <el-card class="user-info-card">
+    <template #header>
+      <div>
+        <span>资产管理</span>
+      </div>
+    </template>
+    <div>
+      <el-empty description="暂未开放" />
+    </div>
+  </el-card>
 </template>
 
 <script setup></script>

cf0220796c3b4a5b25525231714a0ec0a7324f6b
Wyatt <Wyatt6@163.com>
2023-05-20 09:35:42 +0800

    工资福利

--- src/views/finance/salary/index.vue
+++ src/views/finance/salary/index.vue
@@ -1,7 +1,49 @@
 <template>
-  <div>工资福利</div>
+  <div>
+    <el-card class="salary-statistic-card">
+      <div>
+        <el-empty description="统计数据（暂未开放）" />
+      </div>
+    </el-card>
+    <el-tabs type="border-card" class="salary-detail-card" stretch>
+      <el-tab-pane label="工资条">
+        <div>
+          <el-empty description="暂未开放" />
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="五险（养老+医疗+失业+工伤+生育）">
+        <div>
+          <el-empty description="暂未开放" />
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="人寿 & 穗岁康">
+        <div>
+          <el-empty description="暂未开放" />
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="泰康">
+        <div>
+          <el-empty description="暂未开放" />
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="住房公积金">
+        <div>
+          <el-empty description="暂未开放" />
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="企业年金">
+        <div>
+          <el-empty description="暂未开放" />
+        </div>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.salary-statistic-card {
+  margin-bottom: 6px;
+}
+</style>

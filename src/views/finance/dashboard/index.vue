35fa5dcede35486a9eacfc3382717d58ec4a5a23
Wyatt <Wyatt6@163.com>
2023-05-20 09:35:03 +0800

    数据看板

--- src/views/finance/dashboard/index.vue
+++ src/views/finance/dashboard/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>数据看板</div>
+  <el-card class="user-info-card">
+    <template #header>
+      <div>
+        <span>数据看板</span>
+      </div>
+    </template>
+    <div>
+      <el-empty description="暂未开放" />
+    </div>
+  </el-card>
 </template>
 
 <script setup></script>

cb5b9eabe7ca54e6d2d8d0a598f0fa312b6ff91d
Wyatt <Wyatt6@163.com>
2023-05-22 14:16:01 +0800

    收支流水

--- src/views/finance/journal/index.vue
+++ src/views/finance/journal/index.vue
@@ -1,16 +1,27 @@
 <template>
-  <el-card class="user-info-card">
-    <template #header>
-      <div>
-        <span>收支流水</span>
-      </div>
-    </template>
-    <div>
-      <el-empty description="暂未开放" />
-    </div>
-  </el-card>
+  <div>
+    <el-card>
+      <template #header>
+        <div>
+          <span>收支数据</span>
+        </div>
+      </template>
+    </el-card>
+    <el-card class="journal-book">
+      <el-table>
+        <el-table-column label="日期"></el-table-column>
+        <el-table-column label="收支类型"></el-table-column>
+        <el-table-column label="贷方"></el-table-column>
+        <el-table-column label="借方"></el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.journal-book {
+  margin-top: 6px;
+}
+</style>

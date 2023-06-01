1c4d5f4d3308525b63751ebd9b2e45d6ab7de532
Wyatt <Wyatt6@163.com>
2023-06-01 15:40:13 +0800

    更新常量的定义方式

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,19 +1,14 @@
-// 常量
+const env = 'dev'
 
 // ---------- 应用运行有关常量 ----------
-const env = 'dev'
-export const SERVER =
-  env === 'dev' ? 'http://localhost:8000/' : 'http://basic-gateway:8000/'
+export const GATEWAY =
+  env === 'run' ? 'http://basic-gateway:8000/' : 'http://localhost:8000/'
 export const MOCK_ACTIVE = env === 'dev'
+
 // ---------- token有关常量 ----------
-// token
 export const TOKEN = 'token'
-// token时间戳
-export const TOKEN_TIMESTAMP = 'tokenTimestamp'
-// token超时时长，单位：ms
-export const TOKEN_TIMEOUT = 2 * 3600 * 1000 // 2h
+export const TOKEN_EXPIRED_TIME = 'tokenExpiredTime'
+
 // ---------- 组件有关 ----------
-// 侧边栏展开
 export const SIDEBAR_OPENED = 'sidebarOpened'
-// 页面标签列表
 export const VIEW_TAG_LIST = 'viewTagList'

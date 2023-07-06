a2aa0a98497b4d8663243de8a9a60ed6fe9deeae
Wyatt(laptop) <Wyatt6@163.com>
2023-07-06 18:44:36 +0800

    换电脑同步

--- src/api/modules/system.js
+++ src/api/modules/system.js
@@ -1,93 +1,67 @@
 import request from '../request'
 
-const API_PREFIX = '/sys'
-
-/**
- * 获取验证码
- */
+// ---------- Common ----------
+const COMMON = '/sys/common'
 export const getCaptcha = () => {
   return request({
-    url: API_PREFIX + '/common/getCaptcha'
+    url: COMMON + '/getCaptcha'
   })
 }
 
-/**
- * 用户登录
- * @param { username, password, captchaKey, captchaInput } loginForm 登录动作提交的表单
- */
+// ---------- User ----------
+const USER = '/sys/user'
 export const login = (loginForm) => {
   return request({
     method: 'post',
-    url: API_PREFIX + '/user/login',
+    url: USER + '/login',
     data: loginForm
   })
 }
-
-/**
- * 获取用户角色
- */
-export const getRoles = () => {
-  return request({
-    url: API_PREFIX + '/role/getRoles'
-  })
-}
-
-/**
- * 获取用户权限
- */
-export const getAuths = () => {
-  return request({
-    url: API_PREFIX + '/auth/getAuths'
-  })
-}
-
-/**
- * 获取用户个人信息
- */
-export const getProfile = () => {
-  return request({
-    url: API_PREFIX + '/user/getProfile'
-  })
-}
-
-/**
- * 退出登录
- */
 export const logout = () => {
   return request({
-    url: API_PREFIX + '/user/logout'
+    url: USER + '/logout'
   })
 }
-
-/**
- * 获取用户权限详细列表
- */
-export const getAuthDetails = () => {
+export const getRolesOfUser = () => {
   return request({
-    url: API_PREFIX + '/auth/getAuthDetails'
+    url: USER + '/getRolesOfUser'
   })
 }
-
-/**
- * 编辑权限
- * @param {id 必填, identifier, name, description, activated} authForm 提交的表单
- */
-export const editAuth = (authForm) => {
+export const getAuthsOfUser = () => {
   return request({
-    method: 'post',
-    url: API_PREFIX + '/auth/editAuth',
-    data: authForm
+    url: USER + '/getAuthsOfUser'
   })
 }
+export const getProfile = () => {
+  return request({
+    url: USER + '/getProfile'
+  })
+}
+// ---------- Role ----------
 
-/**
- * 新增权限
- * @param {identifier 必填, name, description, activated} authForm 提交的表单
- */
+// ---------- Auth ----------
+const AUTH = '/sys/auth'
 export const addAuth = (authForm) => {
   return request({
     method: 'post',
-    url: API_PREFIX + '/auth/addAuth',
+    url: AUTH + '/addAuth',
     data: authForm
+  })
+}
+export const removeAuth = (id) => {
+  return request({
+    url: AUTH + '/removeAuth?id=' + id
+  })
+}
+export const editAuth = (authForm) => {
+  return request({
+    method: 'post',
+    url: AUTH + '/editAuth',
+    data: authForm
+  })
+}
+export const getAuthList = () => {
+  return request({
+    url: AUTH + '/getAuthList'
   })
 }

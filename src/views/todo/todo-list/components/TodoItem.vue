b9dc8256f0f25e8b2333f2c582d93c02bbb08ba1
Wyatt(laptop) <Wyatt6@163.com>
2023-07-13 18:01:57 +0800

    待办清单初始化

---
+++ src/views/todo/todo-list/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-card height="100%">
+    <div class="button-area">
+      <span class="back">
+        <el-icon :size="25">
+          <Back />
+        </el-icon>
+      </span>
+    </div>
+  </el-card>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.button-area {
+  text-align: left;
+  padding-bottom: 10px;
+
+  .back {
+    cursor: pointer;
+  }
+}
+</style>

1c8c3f86077c3aed927b71c386a29dc44d7c819d
Wyatt(laptop) <Wyatt6@163.com>
2023-07-13 13:49:25 +0800

    财务管理板块

--- src/views/finance/salary/index.vue
+++ src/views/finance/salary/index.vue
@@ -1,30 +1,28 @@
 <template>
-  <div>
-    <el-tabs type="border-card" class="salary-detail-card" stretch>
-      <el-tab-pane label="工资条">
-        <salary-bill></salary-bill>
-      </el-tab-pane>
-      <el-tab-pane label="五险">
-        <insurance></insurance>
-      </el-tab-pane>
-      <el-tab-pane label="商业险（人寿，泰康，穗岁康）">
-        <div>
-          <el-empty description="暂未开放" />
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="住房公积金">
-        <housing-fund></housing-fund>
-      </el-tab-pane>
-      <el-tab-pane label="企业年金">
-        <div>
-          <el-empty description="暂未开放" />
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="工会福利">
-        <welfare></welfare>
-      </el-tab-pane>
-    </el-tabs>
-  </div>
+  <el-tabs type="border-card" class="salary-detail-card" stretch>
+    <el-tab-pane label="工资条">
+      <salary-bill></salary-bill>
+    </el-tab-pane>
+    <el-tab-pane label="五险">
+      <insurance></insurance>
+    </el-tab-pane>
+    <el-tab-pane label="商业险（人寿，泰康，穗岁康）">
+      <div>
+        <el-empty description="暂未开放" />
+      </div>
+    </el-tab-pane>
+    <el-tab-pane label="住房公积金">
+      <housing-fund></housing-fund>
+    </el-tab-pane>
+    <el-tab-pane label="企业年金">
+      <div>
+        <el-empty description="暂未开放" />
+      </div>
+    </el-tab-pane>
+    <el-tab-pane label="工会福利">
+      <welfare></welfare>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script setup>
